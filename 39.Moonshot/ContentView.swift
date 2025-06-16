836ded0d0080a9f87c31a2e5e1ee250ec8dfe989
Valentin <latronixo@gmail.com>
2025-06-16 21:16:22 +0300

    40. Поработал над внешним видом, добавив расширение для ShapeStyle

--- 39.Moonshot/ContentView.swift
+++ 39.Moonshot/ContentView.swift
@@ -26,20 +26,34 @@ struct ContentView: View {
                                     .resizable()
                                     .scaledToFit()
                                     .frame(width: 100, height: 100)
+                                    .padding()
+                                
+                                VStack{
+                                    Text(mission.displayName)
+                                        .font(.headline)
+                                        .foregroundStyle(.white)
+                                    Text(mission.formattedLaunchDate)
+                                        .font(.caption)
+                                        .foregroundStyle(.white.opacity(0.5))
+                                }
+                                .padding(.vertical)
+                                .frame(maxWidth: .infinity)
+                                .background(.lightBackground)
                             }
-                            VStack{
-                                Text(mission.displayName)
-                                    .font(.headline)
-                                Text(mission.formattedLaunchDate)
-                                    .font(.caption)
-                            }
-                            .frame(maxWidth: .infinity)
+                            //}
+                            .clipShape(.rect(cornerRadius: 10))
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 10).stroke(.lightBackground)
+                            )
                         }
                     }
                 }
+                .padding([.horizontal, .bottom])
             }
+            .navigationTitle("Moonshot")
+            .background(.darkBackground)
+            .preferredColorScheme(.dark)
         }
-        .navigationTitle("Moonshot")
     }
 }
 
---
+++ Color-Theme.swift
@@ -0,0 +1,18 @@
+//
+//  Color-Theme.swift
+//  39.Moonshot
+//
+//  Created by Валентин on 16.06.2025.
+//
+
+import SwiftUI
+
+extension ShapeStyle where Self == Color {
+    static var darkBackground: Color {
+        Color(red: 0.1, green: 0.1, blue: 0.2)
+    }
+    
+    static var lightBackground: Color {
+        Color(red: 0.2, green: 0.2, blue: 0.2)
+    }
+}
