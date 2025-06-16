306a6b0318faf74c436c9eff7d5422308d2f16c8
Valentin <latronixo@gmail.com>
2025-06-16 12:06:55 +0300

    Initial Commit

---
+++ 39.Moonshot/_9_MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  _9_MoonshotApp.swift
+//  39.Moonshot
+//
+//  Created by Валентин on 16.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct _9_MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
