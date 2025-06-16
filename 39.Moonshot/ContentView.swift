ff597f497fc30d5d0e74e1aede3cd6a8cd4774ff
Valentin <latronixo@gmail.com>
2025-06-16 13:26:49 +0300

    2. Демонстрация работы ScrollView, для прокручивани за пустое пространство его ширину можно расширить

--- 39.Moonshot/ContentView.swift
+++ 39.Moonshot/ContentView.swift
@@ -9,12 +9,15 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Image(.certificateA2)
-            .resizable()
-            .scaledToFit()
-            .containerRelativeFrame(.horizontal) { size, axis in
-                size * 0.8
+        ScrollView {
+            VStack(spacing: 10) {
+                ForEach(0..<100) {
+                    Text("Item \($0)")
+                        .font(.title)
+                }
             }
+            .frame(maxWidth: .infinity)
+        }
     }
 }
 
