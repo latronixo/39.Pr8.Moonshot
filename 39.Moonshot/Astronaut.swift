c375e2248c2d8073400b172c90888525587dd89b
Valentin <latronixo@gmail.com>
2025-06-16 17:42:40 +0300

    40. 1-2. Добавил миссии и автронавты, декодирование при помощи дженериков из json-файлов

---
+++ 39.Moonshot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  39.Moonshot
+//
+//  Created by Валентин on 16.06.2025.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
--- 39.Moonshot/ContentView.swift
+++ 39.Moonshot/ContentView.swift
@@ -8,20 +8,11 @@
 import SwiftUI
 
 struct ContentView: View {
-    let layout = [
-        GridItem(.adaptive(minimum: 80, maximum: 120)),
-        GridItem(.adaptive(minimum: 80, maximum: 120)),
-        GridItem(.adaptive(minimum: 80, maximum: 120)),
-    ]
-
+    let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
+    let missions: [Mission] = Bundle.main.decode("missions.json")
+    
     var body: some View {
-        ScrollView(.horizontal) {
-            LazyHGrid(rows: layout) {
-                ForEach(0..<1000) {
-                    Text("Item \($0)")
-                }
-            }
-        }
+        Text(String(astronauts.count))
     }
 }
 
