88dc7f93478364c14fbbcb01352517cf523a1902
Valentin <latronixo@gmail.com>
2025-06-16 13:06:16 +0300

    1. Демонстрация задания размера изображения 80% от родительского контейнера

--- 39.Moonshot/ContentView.swift
+++ 39.Moonshot/ContentView.swift
@@ -10,6 +10,11 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         Image(.certificateA2)
+            .resizable()
+            .scaledToFit()
+            .containerRelativeFrame(.horizontal) { size, axis in
+                size * 0.8
+            }
     }
 }
 
